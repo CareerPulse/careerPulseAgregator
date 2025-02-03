4418cf007c8e7b73ea9f4ddc0a86cc81c1ec127d
blacklosh <70694519+blacklosh@users.noreply.github.com>
2025-02-03 15:42:59 +0300

    Transfer repo to company from private

---
+++ hhClient/src/main/java/ru/itis/hhclient/service/ResumeSearchEngine.java
@@ -0,0 +1,12 @@
+package ru.itis.hhclient.service;
+
+import ru.itis.hhclient.dto.ResumeResponseDto;
+import ru.itis.hhclient.gprc.ResumeRequest;
+
+import java.util.List;
+
+public interface ResumeSearchEngine {
+
+    List<ResumeResponseDto> getResumeList(ResumeRequest request);
+    
+}
